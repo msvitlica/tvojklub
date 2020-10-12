1eefd10a7bbb6390933ad20071585f5b0f4a4b01
Nada Jankovic <nadajankovic@gmail.com>
2020-10-12 20:12:35 +0200

    Members component use Data Grid table to render static data

--- backend/routes/members.js
+++ backend/routes/members.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const router = express.Router();
-
 let members = [
-  { _id: 1, name:"Sinisa Kovacevic" },
-  { _id: 2, name:"Milan Svitlica" },
-  { _id: 3, name:"Nada Jankovic" },
-  { _id: 4, name:"Srecko Lazic" },
+  { id: 1, firstName: "Sinisa", lastName: 'Kovacevic', dateOfBirth: '12.04.1988.', group: 'Napredna' },
+  { id: 2, firstName: "Milan", lastName: 'Svitlica', dateOfBirth: '20.06.1987.', group: 'Napredna' },
+  { id: 3, firstName: "Nada", lastName: 'Jankovic', dateOfBirth: '22.10.1986.', group: 'Napredna' },
+  { id: 4, firstName: "Srecko", lastName: 'Lazic', dateOfBirth: '10.11.1990.', group: 'Napredna' },
 ];
-
 router.get("/", async (req, res) => {
-    try {
-      res.status(200).json({
-        members: members
-      });
-    } catch (err) {
-      res.status(400).json({
-        message: "Some error occured",
-        err
-      });
-    }
-  });
- 
-  module.exports = router;
+  try {
+    res.status(200).json({
+      members: members
+    });
+    console.log(members)
+  } catch (err) {
+    res.status(400).json({
+      message: "Some error occured",
+      err
+    });
+  }
+});
+
+module.exports = router;
