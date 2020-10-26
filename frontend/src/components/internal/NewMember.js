eb7c165fef2f24949590744336692ca79a9bc7f6
Nada Jankovic <nadajankovic@gmail.com>
2020-10-26 15:35:29 +0100

    implemented validation in NewMember component

--- frontend/src/components/internal/NewMember.js
+++ frontend/src/components/internal/NewMember.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { TextField, Button, InputLabel, Select, MenuItem, FormControl } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
 const useStyles = makeStyles((theme) => ({
     root: {
         '& .MuiTextField-root': {
@@ -16,7 +15,8 @@ const useStyles = makeStyles((theme) => ({
             marginLeft: theme.spacing(3),
             marginRight: theme.spacing(3),
             width: 200,
-        }}
+        }
+    }
 }));
 export default function NewMember(props) {
     const classes = useStyles();
@@ -24,8 +24,10 @@ export default function NewMember(props) {
         firstName: '',
         lastName: '',
         dateOfBirth: '',
-        group: ''
+        group: '',
     });
+    const [firstNameError, setFirstNameError] = useState({});
+    const [lastNameError, setLastNameError] = useState({});
     const [groups, setGroupList] = useState([]);
     const { history } = props;
     const handleChange = (e) => {
@@ -41,16 +43,35 @@ export default function NewMember(props) {
     useEffect(() => {
         fetchGroup()
     }, [])
-    const onClickSave = () => {
-        if (member.firstName === '' || member.lastName === '') {
-            alert('Popunite ime i prezime.')
+    const validate = () => {
+        const firstNameError = {};
+        const lastNameError = {};
+        let isValid = true;
+
+        if (member.firstName.trim().length < 2) {
+            firstNameError.shortInput = 'Type at least 2 charachters ';
+            firstNameError.notValid = true;
+            isValid = false;
+        }
+        if (member.lastName.trim().length < 2) {
+            lastNameError.shortInput = 'Type at least 2 charachters ';
+            lastNameError.notValid = true;
+            isValid = false;
+        }
+        setFirstNameError(firstNameError);
+        setLastNameError(lastNameError);
+        return isValid;
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const err = validate()
+        if (err) {
             setMember({
                 firstName: member.firstName,
                 lastName: member.lastName,
                 dateOfBirth: member.dateOfBirth,
                 group: member.group,
             })
-        } else {
             postMember();
         }
     }
@@ -73,60 +94,63 @@ export default function NewMember(props) {
         history.push('/members');
     }
     return (
-        <div className={classes.root}>
-                
-                    <TextField
-                        name="firstName"
-                        value={member.firstName}
-                        label="FirstName"
-                        variant="filled"
+        <form className={classes.root} onSubmit={onSubmit} noValidate>
+            <TextField
+                name="firstName"
+                value={member.firstName}
+                label="FirstName"
+                variant="filled"
+                onChange={handleChange}
+                helperText={firstNameError.shortInput}
+                error={firstNameError.notValid}
+            />
+
+            <div>
+                <TextField
+                    name="lastName"
+                    value={member.lastName}
+                    label="LastName"
+                    variant="filled"
+                    onChange={handleChange}
+                    helperText={lastNameError.shortInput}
+                    error={lastNameError.notValid}
+                />
+            </div>
+            <div>
+                <TextField
+                    label="Birthday"
+                    type="date"
+                    name='dateOfBirth'
+                    value={member.dateOfBirth}
+                    onChange={handleChange}
+                    InputLabelProps={{
+                        shrink: true,
+                    }} />
+            </div>
+            <div >
+                <FormControl style={{ width: 220, padding: 13 }}>
+                    <InputLabel shrink >Group</InputLabel>
+                    <Select variant="filled"
+                        name="group"
+                        value={member.group}
                         onChange={handleChange}
-                    />
-            
-                <div>
-                    <TextField
-                        name="lastName"
-                        value={member.lastName}
-                        label="LastName"
-                        variant="filled"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        label="Birthday"
-                        type="date"
-                        name='dateOfBirth'
-                        value={member.dateOfBirth}
-                        onChange={handleChange}
-                        InputLabelProps={{
-                            shrink: true,
-                        }} />
-                </div>
-                <div >
-                <FormControl style= {{width:220,padding:13}}>
-                     <InputLabel shrink >Group</InputLabel> 
-                        <Select variant="filled"
-                            name="group"
-                            value={member.group}
-                            onChange={handleChange}
-                        >
-                            {groups.map(el => (
-                                <MenuItem key={el.id} value={el.name}>{el.name}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </div>
+                    >
+                        {groups.map(el => (
+                            <MenuItem key={el.id} value={el.name}>{el.name}</MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </div>
             <br></br>
             <div>
-                <Button variant="contained" onClick={onClickSave} color="primary">
+                <Button variant="contained" type="submit" color="primary">
                     Save
                         </Button>
                 <Button variant="contained" onClick={onClickCancel} color="secondary">
                     Cancel
                         </Button>
             </div>
-        </div>
+        </form>
 
     )
 }
