6c28ee11543d7cd344c355e4293c2cdd5f5b2d97
Nada Jankovic <nadajankovic@gmail.com>
2020-10-30 14:40:32 +0100

    implemented validation for all fields

--- frontend/src/components/internal/NewMember.js
+++ frontend/src/components/internal/NewMember.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { TextField, Button, InputLabel, Select, MenuItem, FormControl, Grid } from '@material-ui/core';
+import { TextField, Button, InputLabel, Select, MenuItem, FormControl,FormHelperText, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -28,6 +28,8 @@ export default function NewMember(props) {
     });
     const [firstNameError, setFirstNameError] = useState({});
     const [lastNameError, setLastNameError] = useState({});
+    const [birthDateError, setBirthDateError] = useState({});
+    const [groupError, setGroupError] = useState({});
     const [groups, setGroupList] = useState([]);
     const { history } = props;
     const APIurl = 'http://localhost:3001/groups';
@@ -46,19 +48,33 @@ export default function NewMember(props) {
     const validate = () => {
         const firstNameError = {};
         const lastNameError = {};
+        const birthDateError = {};
+        const groupError = {};
         let isValid = true;
         if (member.firstName.trim().length < 2) {
             firstNameError.shortInput = 'Type at least 2 charachters';
             firstNameError.notValid = true;
             isValid = false;
         }
-        if (member.lastName.trim().length < 2) {
+         if (member.lastName.trim().length < 2) {
             lastNameError.shortInput = 'Type at least 2 charachters ';
             lastNameError.notValid = true;
             isValid = false;
         }
+         if (member.dateOfBirth.trim().length === 0) {
+           birthDateError.emptyInput = 'Birth Date is required';
+           birthDateError.notValid = true;
+            isValid = false;
+        }
+        if (member.group.trim().length === 0) {
+            groupError.emptyInput = 'Group is required';
+            groupError.notValid = true;
+             isValid = false;
+         }
         setFirstNameError(firstNameError);
         setLastNameError(lastNameError);
+        setBirthDateError(birthDateError);
+        setGroupError(groupError);
         return isValid;
     }
     const onSubmit = (e) => {
@@ -126,6 +142,8 @@ export default function NewMember(props) {
                             name='dateOfBirth'
                             value={member.dateOfBirth}
                             onChange={handleChange}
+                            helperText= {birthDateError.emptyInput}
+                            error={birthDateError.notValid}
                             InputLabelProps={{
                                 shrink: true,
                             }} />
@@ -137,11 +155,13 @@ export default function NewMember(props) {
                                 name="group"
                                 value={member.group}
                                 onChange={handleChange}
+                                error={groupError.notValid}
                             >
                                 {groups.map(el => (
                                     <MenuItem key={el.id} value={el.name}>{el.name}</MenuItem>
                                 ))}
                             </Select>
+                                <FormHelperText style={{color:'red'}}>{groupError.emptyInput}</FormHelperText>
                         </FormControl>
                     </div>
                     <br></br>
