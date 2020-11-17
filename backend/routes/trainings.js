87fddc04359015278547b060772d34077c58ac20
Nada Jankovic <nadajankovic@gmail.com>
2020-11-17 22:21:03 +0100

    issues in displaying training details

--- backend/routes/trainings.js
+++ backend/routes/trainings.js
@@ -10,16 +10,28 @@ let attendanceStatus= {
 }
 let trainings = [
   {
-    id: uuid.v4(), term: "13-14", group: "Group1", coach: "Sinisa Kovacevic",
+    id: uuid.v4(), term: "13-14", group: "Group1", coach: "Sinisa Kovacevic",date:'2020-11-18',
     membersInGroup: [{name:"Sinisa Kovacevic",id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:"Milan Svitlica",id: uuid.v4(),attendance:attendanceStatus.unknown},{name:"Nada Jankovic",id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:"Srecko Lazic",id: uuid.v4(),attendance:attendanceStatus.unknown}]
   },
   {
-    id: uuid.v4(), term: "15-16", group: "Group3", coach: "Sinisa Kovacevic",
+    id: uuid.v4(), term: "15-16", group: "Group3", coach: "Sinisa Kovacevic",date:'2020-11-16',
+    membersInGroup: [{name:'Rada Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Sofija Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Filip Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}]
+  },
+  {
+    id: uuid.v4(), term: "12-13", group: "Group2", coach: "Sinisa Kovacevic",date:'2020-11-16',
+    membersInGroup: [{name:'Rada Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Sofija Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Filip Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}]
+  },
+  {
+    id: uuid.v4(), term: "16-17", group: "Group4", coach: "Sinisa Kovacevic",date:'2020-11-17',
+    membersInGroup: [{name:'Rada Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Sofija Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Filip Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}]
+  },
+  {
+    id: uuid.v4(), term: "15-16", group: "Group3", coach: "Sinisa Kovacevic",date:'2020-11-17',
     membersInGroup: [{name:'Rada Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Sofija Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Filip Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}]
   },
 ];
 
-router.get("/", async (req, res) => {
+/* router.get("/", async (req, res) => {
   try {
     res.status(200).json({
       trainings: trainings,
@@ -30,12 +42,30 @@ router.get("/", async (req, res) => {
       err
     });
   }
-});
+}); */
+
+//get training by date
+router.get('/:date', async (req,res)=>{
+  let pathName= url.parse(req.url, true).pathname;
+  let pathDate=pathName.replace('/','');
+  let filteredByDate= trainings.filter(el=>el.date === pathDate);
+  console.log(filteredByDate);
+  try {
+    res.status(200).json({
+      training: filteredByDate,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "Some error occured",
+      err
+    });
+  }
+})
 // get group by id
 router.get('/:id', async (req, res) => {
   let pathName = url.parse(req.url, true).pathname;
   let pathId = pathName.replace('/', '');
-  let filteredFile = trainings.filter(el => el.id.toString() === pathId)[0];
+  let filteredFile = trainings.filter(el => el.id=== pathId);
   console.log(filteredFile);
   try {
     res.status(200).json({
@@ -47,8 +77,6 @@ router.get('/:id', async (req, res) => {
       err
     });
   }
-}
-
-);
+});
 
 module.exports = router;
