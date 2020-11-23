495d92827dc80a44b322e04c92ac8dc55f49e92b
Nada Jankovic <nadajankovic@gmail.com>
2020-11-23 20:38:43 +0100

    implemented ScheduleBarDatePicker component

--- backend/routes/trainings.js
+++ backend/routes/trainings.js
@@ -1,53 +1,58 @@
 const express = require("express");
 const router = express.Router();
 const uuid = require('uuid');
-const url = require("url");
- 
-let attendanceStatus= {
+const { format } = require('date-fns');
+
+let attendanceStatus = {
   attended: 'attended',
   noAttended: 'noAttended',
   unknown: 'unknown',
 }
+const getCurrentDate = () => {
+  let date = new Date();
+  const formattedDate = format(date, "yyy-MM-dd");
+  return formattedDate;
+}
+const getYesterdaysDate = () => {
+  let date = new Date();
+  date.setDate(date.getDate() - 1);
+  const formattedDate = format(date, "yyyy-MM-dd");
+  return formattedDate;
+
+}
+const getTomorrowsDate = () => {
+  let date = new Date();
+  date.setDate(date.getDate() + 1);
+  const formattedDate = format(date, "yyyy-MM-dd");
+  return formattedDate;
+}
 let trainings = [
   {
-    id: uuid.v4(), term: "13-14", group: "Group1", coach: "Sinisa Kovacevic",date:'2020-11-18',
-    membersInGroup: [{name:"Sinisa Kovacevic",id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:"Milan Svitlica",id: uuid.v4(),attendance:attendanceStatus.unknown},{name:"Nada Jankovic",id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:"Srecko Lazic",id: uuid.v4(),attendance:attendanceStatus.unknown}]
+    id: uuid.v4(), term: "13-14", group: "Group1", coach: "Sinisa Kovacevic", date: getCurrentDate(),
+    membersInGroup: [{ name: "Sinisa Kovacevic", id: uuid.v4(), attendance: attendanceStatus.unknown }, { name: "Milan Svitlica", id: uuid.v4(), attendance: attendanceStatus.unknown }, { name: "Nada Jankovic", id: uuid.v4(), attendance: attendanceStatus.unknown }, { name: "Srecko Lazic", id: uuid.v4(), attendance: attendanceStatus.unknown }]
   },
   {
-    id: uuid.v4(), term: "15-16", group: "Group3", coach: "Sinisa Kovacevic",date:'2020-11-16',
-    membersInGroup: [{name:'Rada Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Sofija Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Filip Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}]
+    id: uuid.v4(), term: "15-16", group: "Group3", coach: "Sinisa Kovacevic", date: getYesterdaysDate(),
+    membersInGroup: [{ name: 'Rada Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }, { name: 'Sofija Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }, { name: 'Filip Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }]
   },
   {
-    id: uuid.v4(), term: "12-13", group: "Group2", coach: "Sinisa Kovacevic",date:'2020-11-16',
-    membersInGroup: [{name:'Rada Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Sofija Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Filip Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}]
+    id: uuid.v4(), term: "12-13", group: "Group2", coach: "Sinisa Kovacevic", date: getTomorrowsDate(),
+    membersInGroup: [{ name: 'Rada Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }, { name: 'Sofija Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }, { name: 'Filip Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }]
   },
   {
-    id: uuid.v4(), term: "16-17", group: "Group4", coach: "Sinisa Kovacevic",date:'2020-11-17',
-    membersInGroup: [{name:'Rada Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Sofija Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Filip Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}]
+    id: uuid.v4(), term: "16-17", group: "Group4", coach: "Sinisa Kovacevic", date: getCurrentDate(),
+    membersInGroup: [{ name: 'Rada Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }, { name: 'Sofija Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }, { name: 'Filip Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }]
   },
   {
-    id: uuid.v4(), term: "15-16", group: "Group3", coach: "Sinisa Kovacevic",date:'2020-11-17',
-    membersInGroup: [{name:'Rada Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Sofija Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}, {name:'Filip Svitlica',id: uuid.v4(),attendance:attendanceStatus.unknown}]
+    id: uuid.v4(), term: "15-16", group: "Group3", coach: "Sinisa Kovacevic", date: getTomorrowsDate(),
+    membersInGroup: [{ name: 'Rada Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }, { name: 'Sofija Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }, { name: 'Filip Svitlica', id: uuid.v4(), attendance: attendanceStatus.unknown }]
   },
 ];
 
-/* router.get("/", async (req, res) => {
-  try {
-    res.status(200).json({
-      trainings: trainings,
-    });
-  } catch (err) {
-    res.status(400).json({
-      message: "Some error occured",
-      err
-    });
-  }
-}); */
-
 //get training by date
-router.get('/', async (req,res)=>{
-  console.log(req.query.date); 
-  let filteredByDate= trainings.filter(el=>el.date === req.query.date);
+router.get('/', async (req, res) => {
+  console.log(req.query.date);
+  let filteredByDate = trainings.filter(el => el.date === req.query.date);
   console.log(filteredByDate);
   try {
     res.status(200).json({
@@ -62,7 +67,7 @@ router.get('/', async (req,res)=>{
 })
 // get group by id
 router.get('/:id', async (req, res) => {
-  const id = req.params.id;   
+  const id = req.params.id;
   let training = trainings.filter(el => el.id === id)[0];
   console.log(training);
   try {
