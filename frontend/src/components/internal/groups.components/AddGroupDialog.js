e5cdd0aa675f815f2a3b24e1e992c7516ff1a365
nottebianca <sreckolazic94@gmail.com>
2021-01-09 20:38:18 +0100

    created snackbar-service, snackbar component, tested snackbars on groups

--- frontend/src/components/internal/groups.components/AddGroupDialog.js
+++ frontend/src/components/internal/groups.components/AddGroupDialog.js
@@ -1,46 +1,48 @@
-import React, { useState, useEffect,useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import GroupSnackbar from './GroupSnackbar';
-import  {ServiceContext} from './../../../services/ServiceContext'
+import Snackbar from './../snackbar.components/Snackbar';
+import { ServiceContext } from './../../../services/ServiceContext'
 
 export default function AddGroupDialog(props) {
     const [draftGroupName, setDraftGroupName] = useState(props.group.name);
     const [groupNameError, setGroupNameError] = useState('');
-    const [actionPostMessage, setActionPostMessage] = useState('');
-    const [actionPutMessage, setActionPutMessage] = useState('');
-    const [open, setOpen] = useState(false);
-    const services=useContext(ServiceContext);
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+    const [snackbarSeverity, setSeverity] = useState(null);
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+
+    const services = useContext(ServiceContext);
     useEffect(() => {
         setDraftGroupName(props.group.name);
     }, [props.group.name]);
-
     const handleClose = () => {
         props.handleClose();
-    };
+    }
     const onInputChange = (e) => {
         setDraftGroupName(e.target.value);
     }
-    const openSnackbar = () => {
-            setOpen(true);  
-    };
     const closeSnackbar = () => {
-        setOpen(false);
+        setOpenSnackbar(false);
+        setSnackbarMessage('');
     }
 
     const add_edit_Group = async () => {
         if (props.group._id) {
-            const editedData = await services.groupService.editGroup(props.group._id,draftGroupName);
-            setActionPutMessage(editedData);
-            handleClose();
+            const editedData = await services.groupService.editGroup(props.group._id, draftGroupName);
+            const { showSnackbar, msg, severity } = services.snackbarService.showSuccess('Grupa uspješno izmjenjena!');
+            setSnackbarMessage(msg);
+            setSeverity(severity);
+            setOpenSnackbar(showSnackbar);
             setDraftGroupName(draftGroupName);
         } else {
             const postedData = await services.groupService.addGroup(draftGroupName)
-            setActionPostMessage(postedData);
-            handleClose();
+            const { showSnackbar, msg, severity } = services.snackbarService.showSuccess('Grupa uspješno dodana!');
+            setSnackbarMessage(msg);
+            setSeverity(severity);
+            setOpenSnackbar(showSnackbar);
             setDraftGroupName('');
         }
     }
@@ -48,7 +50,7 @@ export default function AddGroupDialog(props) {
         let isValid = true;
         const groupNameError = {};
         if (draftGroupName.trim().length < 2) {
-            groupNameError.emptyInput ='Naziv grupe treba da sadrzi bar 2 karaktera.';
+            groupNameError.emptyInput = 'Naziv grupe treba da sadrzi bar 2 karaktera.';
             groupNameError.notValid = true;
             isValid = false;
         }
@@ -61,8 +63,7 @@ export default function AddGroupDialog(props) {
         if (err) {
             setDraftGroupName(draftGroupName);
             add_edit_Group();
-            openSnackbar();
-            console.log(draftGroupName);
+            handleClose();
         }
     }
     return (
@@ -86,7 +87,7 @@ export default function AddGroupDialog(props) {
                 </DialogActions>
             </Dialog>
             <div>
-                <GroupSnackbar open={open} actionMessage={props.group._id ? actionPutMessage  : actionPostMessage}  closeSnackbar={closeSnackbar}></GroupSnackbar>
+                <Snackbar open={openSnackbar} severity={snackbarSeverity} message={snackbarMessage} onClose={closeSnackbar} />
             </div>
         </div>
     )
