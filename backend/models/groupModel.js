c9d43d95cec01e47c2e9f48f6d3b4be33e32357d
nottebianca <sreckolazic94@gmail.com>
2021-04-20 22:32:08 +0200

    fixed validation messages and when user clicks on editing schedule group field is polulated with chosen group

--- backend/models/groupModel.js
+++ backend/models/groupModel.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const groupSchema = new Schema({
-    name: {
-        type:String,required:true
-    }
-})
+  name: {
+    type: String,
+    required: true
+  },
+});
 
-
-module.exports = mongoose.model('group', groupSchema);
+module.exports = mongoose.model("group", groupSchema);
