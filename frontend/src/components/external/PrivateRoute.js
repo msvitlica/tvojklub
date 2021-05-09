233aad61d0b45b7753cd90129fbd10140ddf7ef1
Slaven B <slaven.bunijevac@gmail.com>
2021-05-10 00:05:35 +0200

    introduce private route context and part of club logic

--- frontend/src/components/external/PrivateRoute.js
+++ frontend/src/components/external/PrivateRoute.js
@@ -4,59 +4,43 @@ import {
     Redirect
 } from 'react-router-dom';
 
-// function PrivateRoute({ component: Component, ...rest }) {
-//     const { isAuthenticated, logout } = rest;
-//     return (
-//         <Route
-//             {...rest}
-//             render={(props) =>
-//                 isAuthenticated ? (
-//                     <Component logout={logout} />
-//                 ) : (
-//                         <Redirect
-//                             to={{
-//                                 pathname: "/login",
-//                                 state: { from: props.location }
-//                             }}
-//                         />
-//                     )
-//             }
-//         />
-//     );
-// }
+export const UserContext = React.createContext();
 
 function PrivateRoute({ component: Component, ...rest }) {
     const [ currentUser, setCurrentUser ] = useState(null);
     const [ fetching, setFetching ] = useState(true);
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const res = await fetch('/api/current_user');
-                if(fetching) {
-                    const user = await res.json();
-                    setCurrentUser(user);
-                }
-                setFetching(false);
-            } catch {
-                console.log('nema korisnika');
+    const fetchUser = async () => {
+        try {
+            const res = await fetch('/api/current_user');
+            if(fetching) {
+                const user = await res.json();
+                setCurrentUser(user);
             }
+            setFetching(false);
+        } catch {
+            console.log('nema korisnika');
         }
+    }
+
+    useEffect(() => {
         fetchUser();    
     }, []);
     
     return (
-        <div>
-            <Route
-                { ...rest } 
-                render={ () => { if (fetching && currentUser === null) return <h1>Fetching...</h1>} }/>
-            <Route
-                { ...rest } 
-                render={ () => { if (currentUser && !fetching) return <Component user={currentUser} />} }/>
-            <Route
-                { ...rest } 
-                render={ () => { if (!currentUser && !fetching) return <Redirect to="/login" />} }/>
-        </div>
+        <UserContext.Provider value={{ currentUser, fetchUser }}>
+            <div>
+                <Route
+                    { ...rest } 
+                    render={ () => { if (fetching && currentUser === null) return <h1>Fetching...</h1>} }/>
+                <Route
+                    { ...rest } 
+                    render={ () => { if (currentUser && !fetching) return <Component user={currentUser} />} }/>
+                <Route
+                    { ...rest } 
+                    render={ () => { if (!currentUser && !fetching) return <Redirect to="/login" />} }/>
+            </div>
+        </UserContext.Provider>
     );
 }
 
--- frontend/src/components/internal/dashboard.component/Dashboard.js
+++ frontend/src/components/internal/dashboard.component/Dashboard.js
@@ -1,43 +1,112 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
+import { UserContext } from '../../external/PrivateRoute';
+
 
 const Dashboard = props => {
+  
+  const { currentUser, fetchUser } = useContext(UserContext);
+  const [ clubName, setClubName] = useState(currentUser.club ? currentUser.club.clubName : '');
+  const [ openDialog, setOpenDialog ] = useState(!!currentUser.club);
+  const [ validationError, setValidationError] = useState({});
+  const [ club, setClub ] = useState(currentUser.club || {});
 
-  const submitClubName = () => {
-    console.log('submitClubName handler', props.user);
+  useEffect(() => {
+    if(!currentUser.club) return;
+    fetchClub();
+  }, []);
+
+  const fetchClub = async () => {
+    const currentClub = await services.clubService.fetchClub();
+    setClub(currentClub);
   }
 
-  const handleClose = () => {
-    console.log('handleClose handler', props.user);
+  const handleChange = e => {
+    setClubName(e.target.value);
+  }
+
+  const validation = () => {
+    let isValidClubName = true;
+    const error = {}
+      if (!clubName || clubName.trim().length < 2) {
+        error.text = 'Naziv kluba mora da sadrži najmanje 2 karaktera.';
+        error.notValid = true;
+        isValidClubName = false;
+      }
+      setValidationError(error);
+      return isValidClubName;
+  }
+
+  const addClub = async () => {
+    const newClub = {
+      owner: currentUser,
+      coaches: [currentUser],
+      newCoaches: []
+    }
+    await services.clubService.addClub(newClub);
+    setOpenDialog(false);
+  }
+
+  const editClub = async () => {
+    const newClub = {};
+    const listOfCoachesIds = [];
+
+    if(club.coaches) listOfCoachesIds = [...club.coaches.map(item => item._id)];
+    
+    if(listOfCoachesIds.includes(currentUser._id)) newClub = { ...club, clubName: clubName }
+    await services.clubService.editClub(newClub);
+    setOpenDialog(false);
+  }
+
+  const addOrEditClub = async () => {
+    if(club.clubId) return editClub();
+    addClub();
+  }
+
+  const submitClub = e => {
+    e.preventDefault();
+    const noError = validation();
+    if(!noError) return;
+    addOrEditClub();
+  }
+
+  const closeDialog = () => {
+    setOpenDialog(false);
   }
   
   return (
     <div>
       <h1>This is my dashboard</h1>
       <h1>Owner: {props.user.name}</h1>
-      <h2>Naziv kluba: {props.user.club || 'moraš unijeti naziv kluba'}</h2>
+      <h2>Naziv kluba: {clubName}</h2>
       <h3>Coaches:</h3>
 
-      <Dialog open={!!props.user.club}>
+      <Dialog open={openDialog}>
         <DialogContent>
           <TextField
             autoFocus
             fullWidth
-            // onChange={onInputChange}
+            onChange={handleChange}
             margin="dense"
-            defaultValue={'tvojklub'}
+            defaultValue={clubName}
             label="Unesite naziv kluba"
-            // helperText={'ovo je naziv kluba'}
+            helperText={validationError.text}
             error={false}
           />
         </DialogContent>
           <DialogActions /* className='dialogButtons' */ style={{ display: 'flex', justifyContent: 'center' }}>
-            <Button onClick={submitClubName} variant='contained' color="primary"> Sačuvaj </Button>
-            <Button onClick={handleClose} variant='contained' color="secondary"> Otkaži </Button>
+            <Button 
+              onClick={submitClub} 
+              variant='contained' 
+              color="primary"> Sačuvaj </Button>
+            <Button 
+              onClick={closeDialog} 
+              variant='contained' 
+              color="secondary"> Otkaži </Button>
           </DialogActions>
       </Dialog>
     </div>
